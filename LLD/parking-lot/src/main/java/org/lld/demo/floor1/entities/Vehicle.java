192ee30ad2860b317fd49955fc5a068374e956bb
Ashfaq <105435085+Ashfaqbs@users.noreply.github.com>
2025-08-15 22:31:33 +0530

    Add Vehicle class to represent vehicles with license plate and type

---
+++ LLD/parking-lot/src/main/java/org/lld/demo/floor1/entities/Vehicle.java
@@ -0,0 +1,14 @@
+package org.lld.demo.floor1.entities;
+
+public class Vehicle {
+    private String licensePlateNumber;
+    private VehicleType vehicleType;
+
+    public Vehicle(String licensePlateNumber, VehicleType vehicleType) {
+        this.licensePlateNumber = licensePlateNumber;
+        this.vehicleType = vehicleType;
+    }
+
+    public String getLicensePlateNumber() { return licensePlateNumber; }
+    public VehicleType getVehicleType() { return vehicleType; }
+}
